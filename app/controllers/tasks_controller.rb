787b30a29f7903902ed7f8925318908b1109453f
EC2 Default User <ec2-user@ip-172-31-12-204.us-east-2.compute.internal>
2018-04-06 12:45:07 +0900

    completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page]).per(10)
   end
   
   def show
@@ -50,6 +50,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :status)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..25).each do |number|
+  Task.create(content: 'test task' + number.to_s, status: '未着手' )
+end
+(26..50).each do |number|
+  Task.create(content: 'test task' + number.to_s, status: '仕掛かり中' )
+end
+(51..75).each do |number|
+  Task.create(content: 'test task' + number.to_s, status: '完了' )
+end
+(76..100).each do |number|
+  Task.create(content: 'test task' + number.to_s, status: '創造的廃棄' )
+end
