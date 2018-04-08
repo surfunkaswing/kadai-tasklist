46dfe38f210992735eb812a5e78b623db589ed5a
EC2 Default User <ec2-user@ip-172-31-12-204.us-east-2.compute.internal>
2018-04-08 23:10:09 +0900

    login function v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+User.create(name: 'testuser1', email: 'TestUser1@tasklist.com', password: 'test1234')
+User.create(name: 'testuser2', email: 'TestUser2@tasklist.com', password: 'test2345')
+User.create(name: 'testuser3', email: 'TestUser3@tasklist.com', password: 'test3456')
+User.create(name: 'testuser4', email: 'TestUser4@tasklist.com', password: 'test4567')
+User.create(name: 'testuser5', email: 'TestUser5@tasklist.com', password: 'test5678')
+
+
 (1..25).each do |number|
-  Task.create(content: 'test task' + number.to_s, status: '未着手' )
+  Task.create(content: 'test task' + number.to_s, status: '未着手', user_id: 1 )
 end
 (26..50).each do |number|
-  Task.create(content: 'test task' + number.to_s, status: '仕掛かり中' )
+  Task.create(content: 'test task' + number.to_s, status: '仕掛かり中', user_id: 2 )
 end
 (51..75).each do |number|
-  Task.create(content: 'test task' + number.to_s, status: '完了' )
+  Task.create(content: 'test task' + number.to_s, status: '完了', user_id: 3 )
 end
 (76..100).each do |number|
-  Task.create(content: 'test task' + number.to_s, status: '創造的廃棄' )
+  Task.create(content: 'test task' + number.to_s, status: '創造的廃棄', user_id: 4 )
+end
+(101..125).each do |number|
+  Task.create(content: 'test task' + number.to_s, status: '創造的廃棄', user_id: 5 )
 end
